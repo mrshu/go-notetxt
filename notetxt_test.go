a6e0e36fe8e605adfe90026562b954b3e9a74672
mr.Shu <mr@shu.io>
2014-05-11 22:41:09 +0200

    Comments for docs

--- notetxt_test.go
+++ notetxt_test.go
@@ -6,12 +6,20 @@ import (
 )
 
 func TestTitleToFile (t *testing.T) {
+
+        // a normal title
         in1 := "Some interesting title"
         assert.Equal(t, TitleToFilename(in1), "some-interesting-title")
 
+        // a title with some funny characters
         in2 := "SomeName: a nice Name"
         assert.Equal(t, TitleToFilename(in2), "somename-a-nice-name")
 
+        // a name full of funny characters
         in3 := "HaHaHa !! funny characters ,, .af425-1.q@22@#%^^&**@!"
         assert.Equal(t, TitleToFilename(in3), "hahaha-funny-characters-.af425-1.q22")
+
+        // a name consisting of funny characters only
+        in4 := "!@#$%^&*())!@#$%^^&!$@#%@^$%@$#$@#$@"
+        assert.Equal(t, TitleToFilename(in4), "")
 }
